220a8917b1fe7835b82893ab6111af4b66055935
Boris Hetsko <borishetsko@gmail.com>
2018-12-06 17:44:26 +0200

    Revert "Change slider breakpoint"
    This reverts commit 483ba6b5abcdafbfc741bae36a7d108fb7c325f0.

--- Apptastico/js/main.js
+++ Apptastico/js/main.js
@@ -12,7 +12,7 @@
 		  	slidesToScroll: 1,
 		  	responsive: [
 			    {
-			      breakpoint: 1300,
+			      breakpoint: 1200,
 			      settings: {
 			      	slidesToShow: 3,
 			      	slidesToScroll: 1,
