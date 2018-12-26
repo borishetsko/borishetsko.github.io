5bf6d8060bf9073fac51af40e41cb2e470d32f64
Boris Hetsko <borishetsko@gmail.com>
2018-12-26 16:52:14 +0200

    Add scroll class

--- Web&GraphicDesigner/js/template.js
+++ Web&GraphicDesigner/js/template.js
@@ -11,22 +11,20 @@
 		  	slidesToShow: 1,
 		  	slidesToScroll: 1
 	});
-
-	// (function($){
-	// $(document).ready(function(){
-	// 	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
-	// 		event.preventDefault(); 
-	// 		event.stopPropagation(); 
-	// 		$(this).parent().siblings().removeClass('open');
-	// 		$(this).parent().toggleClass('open');
-	// 	});
-	// });
-	// })(jQuery);
-
+		
 	$("body").on("click", ".to-top a, .main-nav li a, .slider a", function() {
 		var idtop = $($(this).attr("href")).offset().top;
 		$('html,body').animate({scrollTop: idtop}, 1500);
 		return false;
+	});
+
+	var menu = $('#menu').offset().top;
+	$(window).scroll(function(){
+		if($(window).scrollTop() >= menu) {
+			$('.to-top').addClass('vis');
+		} else {
+			$('.to-top').removeClass('vis');
+		}
 	});
 
 	})
