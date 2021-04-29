1db1a49ac5a876d5c30b70cc1bfd0afad29a2370
Boris <borishetsko@gmail.com>
2021-04-29 14:49:14 +0300

    Add test project
    twnty test task

---
+++ twnty_test/js/main.js
@@ -0,0 +1,20 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.menu');
+const body = document.querySelector('body');
+let showMenu = false;
+
+burger.addEventListener('click', onBurgerClick);
+
+function onBurgerClick () {
+  if (!showMenu) {
+    burger.classList.add('active');
+    nav.classList.add('active');
+    body.classList.add('lock');
+    showMenu = true;
+  } else {
+    burger.classList.remove('active');
+    nav.classList.remove('active');
+    body.classList.remove('lock');
+    showMenu = false;
+  }
+}
